b391663640256202079727173bca50bf92fbee76
Tuấn <ntuan9870@gmail.com>
2021-03-12 21:18:01 +0700

    Cấu hình Retrofit -- Cái này dùng để kết nối với API

---
+++ app/src/main/java/com/example/t2shop/CallBack/RetrofitClient.java
@@ -0,0 +1,18 @@
+package com.example.t2shop.CallBack;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit;
+    public static Retrofit getInstance(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder().baseUrl("http://localhost:8000/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}

590767dcb70e7e14cdd5b91cfbc5277b68bade5e
Tuấn <ntuan9870@gmail.com>
2021-06-06 13:34:37 +0700

    xem chi tiết voucher

--- app/src/main/java/com/example/t2shop/Adapter/VoucherAdapter.java
+++ app/src/main/java/com/example/t2shop/Adapter/VoucherAdapter.java
@@ -1,5 +1,6 @@
 package com.example.t2shop.Adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +10,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -53,6 +56,27 @@ public class VoucherAdapter extends RecyclerView.Adapter<VoucherAdapter.ViewHold
                 transaction.commit();
             }
         });
+        holder.btn_view_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mSuccess = new AlertDialog.Builder(context);
+                View mView = ((Activity)context).getLayoutInflater().inflate(R.layout.dialog_detail_voucher, null);
+                Button btn_ok = mView.findViewById(R.id.btn_ok);
+                TextView txt_detail_voucher = mView.findViewById(R.id.txt_detail_voucher);
+                mSuccess.setView(mView);
+                AlertDialog dialog = mSuccess.create();
+                dialog.show();
+                txt_detail_voucher.setText("Tên Voucher: " + vouchers.get(position).getVoucher_name()+"\n"
+                +"Ngày hết hạn: "+vouchers.get(position).getVoucher_end() + "\nGiá trị: "+vouchers.get(position).getVoucher_value()
+                +"\nChiết khấu: " + vouchers.get(position).getVoucher_discount() + "\nSố lượng: " +  vouchers.get(position).getVoucher_total());
+                btn_ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -62,11 +86,12 @@ public class VoucherAdapter extends RecyclerView.Adapter<VoucherAdapter.ViewHold
 
     class ViewHolder extends RecyclerView.ViewHolder {
         private TextView txt_name_voucher;
-        private Button btn_use;
+        private Button btn_use, btn_view_detail;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txt_name_voucher = itemView.findViewById(R.id.txt_name_voucher);
             btn_use = itemView.findViewById(R.id.btn_use);
+            btn_view_detail = itemView.findViewById(R.id.btn_view_detail);
         }
     }
 }
